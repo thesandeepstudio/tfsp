5bea04d0e32b2ac84bda0070fbb98026ca98fecf
Sandeep <thesandeep2080@gmail.com>
2026-07-02 22:03:04 +0545

    Revert "Push hero video down ~85px to clear the sticky navbar"
    This reverts commit 17bbdd2b26a7e82ddf9746607d7a4c1537155a5c.

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,7 +2,7 @@ import HeroVideo from "@/components/HeroVideo";
 
 export default function Hero() {
   return (
-    <section className="relative mt-21.25 flex aspect-video items-end overflow-hidden bg-neutral-800 px-4 pb-16 pt-32 sm:px-8">
+    <section className="relative flex aspect-video items-end overflow-hidden bg-neutral-800 px-4 pb-16 pt-32 sm:px-8">
       <HeroVideo />
     </section>
   );
